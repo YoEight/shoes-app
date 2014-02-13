d1cbd8c6376db961a58d07948c507fc26041d86f
YoEight <yo.eight@gmail.com>
2014-02-13 22:03:42 +0100

    Implement FromJSON instance of Shoes and NewShoes

--- lib/Shoes/Model.hs
+++ lib/Shoes/Model.hs
@@ -1,9 +1,11 @@
-{-# LANGUAGE FlexibleContexts   #-}
-{-# LANGUAGE GADTs              #-}
-{-# LANGUAGE OverloadedStrings  #-}
-{-# LANGUAGE QuasiQuotes        #-}
-{-# LANGUAGE TemplateHaskell    #-}
-{-# LANGUAGE TypeFamilies       #-}
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE GADTs                #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE QuasiQuotes          #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Shoes.Model
@@ -17,8 +19,15 @@
 ----------------------------------------------------------------------------
 module Shoes.Model where
 
-import Database.Persist.TH
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (mzero)
+
+import Data.Aeson hiding (decode)
+import Data.ByteString (ByteString)
+import Data.ByteString.Base64 (decode)
 import Data.Text (Text)
+import Data.Text.Encoding (encodeUtf8)
+import Database.Persist.TH
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 Shoes
@@ -30,3 +39,29 @@ Photo
     shoes ShoesId
     filePath Text
 |]
+
+data NewShoes
+    = NewShoes
+      { newShoes :: !Shoes
+      , newPhoto :: !ByteString
+      }
+
+instance FromJSON Shoes where
+    parseJSON (Object m) =
+        Shoes              <$>
+        m .: "description" <*>
+        m .: "color"       <*>
+        m .: "size"
+    parseJSON _ = mzero
+
+instance FromJSON NewShoes where
+    parseJSON p@(Object m) =
+        NewShoes    <$>
+        parseJSON p <*>
+        (m .: "photo" >>= go)
+      where
+        go txt =
+            case decode $ encodeUtf8 txt of
+                Left e      -> fail e
+                Right bytes -> return bytes
+    parseJSON _ = mzero
