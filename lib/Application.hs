0a3f86cc3a931b0c63ee3fba28e87a65fd69c36d
YoEight <yo.eight@gmail.com>
2014-02-14 16:48:34 +0100

    Appease -Werror

--- lib/Application.hs
+++ lib/Application.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Application
