e37d0d488ebd3d744d73eaa0e3c6558dcd5e0062
YoEight <yo.eight@gmail.com>
2014-02-14 16:49:13 +0100

    Implement postNewShoesR

--- lib/Shoes.hs
+++ lib/Shoes.hs
@@ -12,37 +12,37 @@
 -- Portability :  non-portable
 --
 ----------------------------------------------------------------------------
-module Shoes where
+module Shoes
+       ( getShoesR
+       , getShoesListR
+       , postNewShoesR
+       ) where
 
 import Control.Applicative ((<$>), (<*>))
 import Control.Monad (mzero)
+import Data.Int (Int64)
 
 import Data.Aeson hiding (decode)
-import Data.ByteString (ByteString)
+import Data.Aeson.Types (Parser)
+import Data.Attoparsec.ByteString hiding (Parser)
+import Data.Conduit
+import Data.Conduit.Binary
+import Data.ByteString (ByteString, empty)
 import Data.ByteString.Base64 (decode)
+import Data.Maybe (fromMaybe)
+import Data.Text (pack)
 import Data.Text.Encoding (encodeUtf8)
+import Yesod
 
 import Foundation
 import Shoes.Model
 
-data NewShoes
-    = NewShoes
-      { newShoes :: !Shoes
-      , newPhoto :: !ByteString
-      }
-
-instance FromJSON Shoes where
-    parseJSON (Object m) =
-        Shoes              <$>
-        m .: "description" <*>
-        m .: "color"       <*>
-        m .: "size"
-    parseJSON _ = mzero
+data NewShoes = NewShoes !Shoes !ByteString -- < photo bytes
 
 instance FromJSON NewShoes where
     parseJSON p@(Object m) =
-        NewShoes    <$>
-        parseJSON p <*>
+        NewShoes         <$>
+        parseShoesJSON p <*>
         (m .: "photo" >>= go)
       where
         go txt =
@@ -51,11 +51,51 @@ instance FromJSON NewShoes where
                 Right bytes -> return bytes
     parseJSON _ = mzero
 
-postNewShoesR :: Handler Value
-postNewShoesR = undefined
+parseShoesJSON :: Value -> Parser Shoes
+parseShoesJSON (Object m) =
+    Shoes              <$>
+    m .: "description" <*>
+    m .: "color"       <*>
+    m .: "size"
+parseShoesJSON _ = mzero
+
+postNewShoesR :: Handler ()
+postNewShoesR = do
+    value <- rawRequestBody $$ sinkJson
+    case fromJSON value of
+        Error e   -> invalidArgs [pack e]
+        Success n -> saveNewShoes n
 
 getShoesR :: ShoesId -> Handler Value
 getShoesR = undefined
 
 getShoesListR :: Handler Value
 getShoesListR = undefined
+
+sinkJson :: Sink ByteString Handler Value
+sinkJson = do
+    bOpt <- await
+    res  <- parseWith action json' (fromMaybe empty bOpt)
+    either reportError return (eitherResult res)
+  where
+    action = fmap (fromMaybe empty) await
+
+    reportError e =
+        invalidArgs [pack e]
+
+saveNewShoes :: NewShoes -> Handler ()
+saveNewShoes (NewShoes shoes photoBin) =
+    runDB $ do
+        nsId <- insert shoes
+        let key      = keyToInt nsId
+            filepath = "repo/" ++ show key ++ ".jpeg"
+            photo    = Photo nsId (pack filepath)
+            source   = yield photoBin
+        liftIO $ runResourceT (source $$ sinkFile filepath)
+        insert_ photo
+
+keyToInt :: ShoesId -> Int64
+keyToInt = go . unKey
+  where
+    go (PersistInt64 i) = i
+    go _                = error "impossible situation in Shoes.hs"
